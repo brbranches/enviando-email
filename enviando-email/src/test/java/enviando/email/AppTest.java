ec04c0074bfc765a5fd9a64a8c8ca67d61a590a7
brbranches <brunojramosr@gmail.com>
2021-05-07 16:35:56 -0300

    Configurando conexão com servidor Gmail

--- enviando-email/src/test/java/enviando/email/AppTest.java
+++ enviando-email/src/test/java/enviando/email/AppTest.java
@@ -2,23 +2,44 @@ package enviando.email;
 
 import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
-    
-  @Test
-  public void testeEmail() {
- 
-	  Properties properties = new Properties();
-	  properties.put("mail.smtp.auth", "true"); /*Autorização*/
-	  properties.put("mail.smtp.starttls", "true");/*Autenticação*/
-	  properties.put("mail.smtp.host", "smtp.gmail.com");/*Servidor gmail do Google*/
-	  properties.put("mail.smtp.prt", "465");/*Porta do servidor*/
-	  properties.put("mail.smtp.socketFactory.port", "465");/*Especifica a Porta a ser conectada pelo socket*/
-	  properties.put("mail.smtp.socketFactory.Class", "javax.net.ssl.SSLSocketFactory");/*Classe socket de conexão ao SMTP*/
-	  
-	  
-	  
-  
-  }
+
+	private String userName = "brunoramos.testes@gmail.com";
+	private String password = "Elmt2021*";
+
+	@Test
+	public void testeEmail() {
+
+		try {
+			Properties properties = new Properties();
+			properties.put("mail.smtp.auth", "true"); /* Autorização */
+			properties.put("mail.smtp.starttls", "true");/* Autenticação */
+			properties.put("mail.smtp.host", "smtp.gmail.com");/* Servidor gmail do Google */
+			properties.put("mail.smtp.prt", "465");/* Porta do servidor */
+			properties.put("mail.smtp.socketFactory.port", "465");/* Especifica a Porta a ser conectada pelo socket */
+			properties.put("mail.smtp.socketFactory.Class",
+					"javax.net.ssl.SSLSocketFactory");/* Classe socket de conexão ao SMTP */
+
+			Session session = Session.getDefaultInstance(properties, new Authenticator() {
+
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(userName, password);
+				}
+
+			});
+
+			System.out.println(session);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
